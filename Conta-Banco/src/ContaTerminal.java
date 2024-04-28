a5e6332cb0a0327c0e25fc81fe4469e215239a37
Willian Pero <wilhampero@hotmail.com>
2024-04-27 22:27:12 -0300

    atividade dio

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println(" Por Favor, Digite sua Conta.");
+        Double conta = scanner.nextDouble();
+
+        System.out.println("Por Favor, Digite sua Agência.");
+        String agencia = scanner.next();
+
+        System.out.println("Por Favor, Digite seu Nome.");
+        String nome = scanner.next();
+
+        System.out.println("Por Favor, Digite seu Depósito.");
+        String Deposito = scanner.next();
+
+}
+
 }
